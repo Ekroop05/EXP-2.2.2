790249c930a0c5e48a22f307ce62923183380913
Ekroop05 <ekroopsingh2005@gmail.com>
2026-03-18 17:41:20 +0530

    Update

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,7 @@
+import User from "../models/User.js";
+import bcrypt from "bcryptjs";
+import generateToken from "../utils/generateToken.js";
+
 export const registerUser = async (req, res) => {
   try {
     const { name, email, password } = req.body;
@@ -20,6 +24,27 @@ export const registerUser = async (req, res) => {
       message: "User registered",
       token: generateToken(user._id),
     });
+
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+
+export const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+      res.json({
+        message: "Login successful",
+        token: generateToken(user._id),
+      });
+    } else {
+      res.status(401).json({ message: "Invalid credentials" });
+    }
 
   } catch (error) {
     res.status(500).json({ message: error.message });
